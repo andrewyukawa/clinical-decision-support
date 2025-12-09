219c1e8a14eb5d546ab553f923e620ed1861428b
Andrew Yukawa <andrew.yukawa@zoomrx.com>
2025-12-09 15:27:49 -0800

    Add Railway deployment configuration (nixpacks.toml)

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup file for Railway deployment"""
+from setuptools import setup
+
+setup(
+    name="curie-cds",
+    version="1.0.0",
+    description="Curie Clinical Decision Support System",
+    python_requires=">=3.8",
+)
+
